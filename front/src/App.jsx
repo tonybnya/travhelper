18e1f7e25c426d1ecd941e0e4ca7a3c33d0cc749
Tony <nya.tony2010@gmail.com>
2023-08-28 13:56:14 +0100

    Add: some assets

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold">
+    <h1 className="text-3xl font-bold underline text-cyan-600">
       Hello world!
     </h1>
   )
