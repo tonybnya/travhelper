903439cdb16117e6010f2ba70a23ece04e55e0fc
Tony <nya.tony2010@gmail.com>
2023-08-28 14:12:15 +0100

    Add: additional assets

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-cyan-600">
-      Hello world!
-    </h1>
-  )
-}
+import { Navbar, Hero, Footer } from './components';
+
+const App = () => (
+  <main className="relative">
+    <Navbar />
+
+    <section>
+      <Hero />
+    </section>
+
+    <section>
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ front/src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Footer from './Footer';
+
+export {
+  Navbar,
+  Hero,
+  Footer
+};
